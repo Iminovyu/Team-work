73d4271ea0d05394c4b6df3a53e7b4156f8fafe5
Iminovyu <iminovdavronbek521@gmail.com>
2024-08-18 11:30:34 +0500

    wda

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,31 @@
   <div>
     <section class="main-info">
       <div class="container">
-        
+        <div class="product-filter">
+          <div class="product-count">
+            <h1>Найденно товаров: <span>245</span></h1>
+          </div>
+          <div class="product-types">
+            <label for="radio">
+              <input type="radio" name="product-type" id="radio" checked>
+              <span>Все</span>
+            </label>
+            <label for="radio2">
+              <input type="radio" name="product-type" id="radio2">
+              <span>Новинки</span>
+            </label>
+            <label for="radio3">
+              <input type="radio" name="product-type" id="radio3">
+              <span>Со скидкой</span>
+            </label>
+          </div>
+          <div class="product-select">
+            <select name="select" class="select">
+              <option value="Сначало дешевле">Сначало дешевле</option>
+              <option value="Сначало дороже">Сначало дороже</option>
+            </select>
+          </div>
+        </div>
       </div>
     </section>
   </div>
