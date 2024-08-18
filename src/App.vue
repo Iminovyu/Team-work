e43f771fd5d4f596c2fa125f0f9b252961759862
Iminovyu <iminovdavronbek521@gmail.com>
2024-08-18 16:28:54 +0500

    JNJ

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import { RouterLink, RouterView } from 'vue-router';
 import NavComponent from './components/NavComponent.vue';
 import HeroComponent from './components/HeroComponent.vue';
 import MainComponent from './components/MainComponent.vue';
-
+</script>
 <style lang="scss" scoped>
 
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,86 +1,107 @@
 <template>
-      <div>
-            <div class="container">
-                  <div class="product-filter">
-                        <div class="product-count">
-                              <h1>Найденно товаров: <span>245</span></h1>
-                        </div>
-                        <div class="product-types">
-                              <label for="radio">
-                                    <input type="radio" name="product-type" id="radio" checked>
-                                    <span class="custom-radio"></span>
-                                    <span>Все</span>
-                              </label>
-                              <label for="radio2">
-                                    <input type="radio" name="product-type" id="radio2">
-                                    <span class="custom-radio"></span>
-                                    <span>Новинки</span>
-                              </label>
-                              <label for="radio3">
-                                    <input type="radio" name="product-type" id="radio3">
-                                    <span class="custom-radio"></span>
-                                    <span>Со скидкой</span>
-                              </label>
-                        </div>
-                        <div class="product-select">
-                              <select name="select" class="select">
-                                    <option value="Сначало дешевле">Сначало дешевле</option>
-                                    <option value="Сначало дороже">Сначало дороже</option>
-                              </select>
-                        </div>
-                  </div>
-                  <div class="left-nav">
-                        <div class="product-sections">
-                              <router-link to="/">Все товары</router-link>
-                              <router-link to="/">Афганские скороварки</router-link>
-                              <router-link to="/">Казаны и котлы</router-link>
-                              <router-link to="/">Учаги</router-link>
-                              <router-link to="/">Кастрюли</router-link>
-                              <router-link to="/">Мантоварки</router-link>
-                              <router-link to="/">Сковороды</router-link>
-                              <router-link to="/">Термосы</router-link>
-                              <router-link to="/">Чайники и френч-прессы</router-link>
-                              <router-link to="/">Сервировочная посуда</router-link>
-                              <router-link to="/">Столовые приборы</router-link>
-                              <router-link to="/">Бытовая техника</router-link>
-                              <router-link to="/">Бытовая химия</router-link>
-                              <router-link to="/">Товары для дома и сада</router-link>
-      
-      
-                        </div>
-                        <div class="filter-byPrice">
-                              <div class="sec-control">
-                                    <h1>Цена</h1>
-                                    <transition name="rotate">
-                                          <div class="img">
-                                                <img src="@/assets/images/svg/arrow.svg" alt="arrow">
-                                          </div>
-                                    </transition>
-                              </div>
-                              <transition class="appear">
-                                    <div class="hidenshow">
-                                          <p>от <span>0</span> до <span>5000</span></p>
-                                          <div class="slider">
-                                                <div class="min"></div>
-                                                <div class="max"></div>
-                                          </div>
-                                    </div>
-                              </transition>
-                        </div>
-                  </div>
-            </div>
-            <div class="cards">
-                <div class="card" v-for="card in cards" :key="card">
-                    <img :src="card.url" alt="">
-                    <p>{{ card.name }}</p>
-                    <div class="price">
-                        <p>{{ card.oldprice }}</p>
-                        <p>{{ card.newprice }}</p>
-                    </div>
+    <div>
+        <div class="container">
+            <div class="product-filter">
+                <div class="product-count">
+                    <h1>Найденно товаров: <span>245</span></h1>
+                </div>
+                <div class="product-types">
+                    <label for="radio">
+                        <input type="radio" name="product-type" id="radio" checked>
+                        <span class="custom-radio"></span>
+                        <span>Все</span>
+                    </label>
+                    <label for="radio2">
+                        <input type="radio" name="product-type" id="radio2">
+                        <span class="custom-radio"></span>
+                        <span>Новинки</span>
+                    </label>
+                    <label for="radio3">
+                        <input type="radio" name="product-type" id="radio3">
+                        <span class="custom-radio"></span>
+                        <span>Со скидкой</span>
+                    </label>
+                </div>
+                <div class="product-select">
+                    <select name="select" class="select">
+                        <option value="Сначало дешевле">Сначало дешевле</option>
+                        <option value="Сначало дороже">Сначало дороже</option>
+                    </select>
                 </div>
             </div>
-
-</div>
+            <div class="products">
+                <div class="left-nav">
+                    <div class="product-sections">
+                        <router-link to="/">Все товары</router-link>
+                        <router-link to="/">Афганские скороварки</router-link>
+                        <router-link to="/">Казаны и котлы</router-link>
+                        <router-link to="/">Учаги</router-link>
+                        <router-link to="/">Кастрюли</router-link>
+                        <router-link to="/">Мантоварки</router-link>
+                        <router-link to="/">Сковороды</router-link>
+                        <router-link to="/">Термосы</router-link>
+                        <router-link to="/">Чайники и френч-прессы</router-link>
+                        <router-link to="/">Сервировочная посуда</router-link>
+                        <router-link to="/">Столовые приборы</router-link>
+                        <router-link to="/">Бытовая техника</router-link>
+                        <router-link to="/">Бытовая химия</router-link>
+                        <router-link to="/">Товары для дома и сада</router-link>
+    
+    
+                    </div>
+                    <div class="filter-byPrice">
+                        <div class="sec-control">
+                            <h1>Цена</h1>
+                            <transition name="rotate">
+                                <div class="img">
+                                    <img src="@/assets/images/svg/arrow.svg" alt="arrow">
+                                </div>
+                            </transition>
+                        </div>
+                        <transition class="appear">
+                            <div class="hidenshow">
+                                <p>от <span>0</span> до <span>5000</span></p>
+                                <div class="slider">
+                                    <div class="min"></div>
+                                    <div class="max"></div>
+                                </div>
+                            </div>
+                        </transition>
+                    </div>
+                </div>
+    
+                <div class="cards">
+                    <div class="card" v-for="card in cards" :key="card">
+                        <img :src="card.url" alt="">
+                        <p>{{ card.name }}</p>
+                        <div class="price">
+                            <p>{{ card.oldprice }}</p>
+                            <p>{{ card.newprice }}</p>
+                        </div>
+                    </div>
+                    <div class="card" v-for="card in cards" :key="card">
+                        <img :src="card.url" alt="">
+                        <p>{{ card.name }}</p>
+                        <div class="price">
+                            <p>{{ card.oldprice }}</p>
+                            <p>{{ card.newprice }}</p>
+                        </div>
+                    </div>
+                    <div class="card" v-for="card in cards" :key="card">
+                        <img :src="card.url" alt="">
+                        <p>{{ card.name }}</p>
+                        <div class="price">
+                            <p>{{ card.oldprice }}</p>
+                            <p>{{ card.newprice }}</p>
+                        </div>
+                    </div>
+                </div>
+    
+    
+            </div>
+        </div>
+    
+    </div>
 </template>
 
 <script setup>
@@ -96,17 +117,29 @@ axios.get('https://full-api.onrender.com/api/product/get')
   .catch((err) => {
     console.log(err)
   })
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/pagination';
+
+
+  // import required modules
+  import { Pagination } from 'swiper/modules';
 </script>
 
 <style lang="scss" scoped>
-
 $red-color: rgba(245, 85, 85, 1);
 $gray-color: rgba(196, 196, 196, 1);
+
 .product-filter {
     display: flex;
     justify-content: space-between;
     padding: 20px;
     background: rgba(255, 255, 255, 1);
+    margin-top: 25px;
 
     .product-count {
         h1 {
@@ -196,6 +229,7 @@ $gray-color: rgba(196, 196, 196, 1);
     flex-direction: column;
     background: #fff;
     width: 212px;
+
     .product-sections {
         display: flex;
         padding: 20px;
@@ -206,6 +240,7 @@ $gray-color: rgba(196, 196, 196, 1);
         width: inherit;
         padding-bottom: 20px;
         border-bottom: 1px solid $gray-color;
+
         a {
             font-size: 14px;
             font-weight: 700;
@@ -219,7 +254,8 @@ $gray-color: rgba(196, 196, 196, 1);
             }
         }
     }
-    .filter-byPrice{
+
+    .filter-byPrice {
         display: flex;
         flex-direction: column;
         padding: 20px;
@@ -229,7 +265,8 @@ $gray-color: rgba(196, 196, 196, 1);
         width: inherit;
         padding-bottom: 20px;
         border-bottom: 1px solid $gray-color;
-        h1{
+
+        h1 {
             font-size: 14px;
             font-weight: 700;
             line-height: 22px;
@@ -237,11 +274,45 @@ $gray-color: rgba(196, 196, 196, 1);
             text-decoration: none;
             color: #000;
         }
-        .sec-control{
+
+        .sec-control {
             display: flex;
             align-items: center;
             gap: 10px;
-            .img{
+
+        }
+    }
+}
+
+.products {
+    margin-top: 25px;
+    display: flex;
+    justify-content: space-between;
+
+    .cards {
+        margin-top: 50px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 30px;
+
+        .card {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            width: 229px;
+            height: 240px;
+            top: 675px;
+            left: 859px;
+            gap: 0px;
+            opacity: 0px;
+
+            img {
+                width: 229px;
+                height: 180px;
+                top: 675px;
+                left: 353px;
+                gap: 0px;
+                opacity: 0px;
 
             }
         }
